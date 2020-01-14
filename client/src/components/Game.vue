ff2ab21c91bf75fbdc8efe517f79f720ad4a77e1
Jaakko Uusitalo <jaakkouusitalo@outlook.com>
2020-01-14 19:26:34 +0200

    added ability to press play

--- client/src/components/Game.vue
+++ client/src/components/Game.vue
@@ -1,18 +1,10 @@
 <template>
     <div ref="game">
-        <div id="header">
-            <div id="player1">
-                <div class="playerName">Player 1</div>
-                <div class="playerScore">20</div>
-            </div>
-            <div id="divider">{{ game.score }}</div>
-            <div id="player2">
-                <div class="playerName">Player 2</div>
-                <div class="playerScore">20</div>
-            </div>
-        </div>
-        <div id="content">
-            <button v-on:click="play">Play</button>
+        <div class="statusBar">People playing: {{ clientCount }}</div>
+        <div class="game">
+            <h1>{{ points }}</h1>
+            <p>points</p>
+            <button v-on:click="play()">Play</button>
         </div>
     </div>
 </template>
@@ -24,25 +16,66 @@
             return {
                 socket: {},
                 context: {},
-                game: {
-                    score: 20
-                }
+                points: 0,
+                clientCount: 0
             }
         },
         created() {
             this.socket = io('http://localhost:3000');
+            this.socket.on('update points', data => {
+                this.points = data;
+            })
+            this.socket.on("client count", data => {
+                this.clientCount = data;
+            })
         },
         mounted() {
             this.context = this.$refs.game;
-            this.socket.on('game', data => {
-                this.game = data;
-            });
         },
         methods: {
             play() {
-                this.socket.emit('play');
+                this.socket.emit('play', this.points);
             }
         }
     }
 </script>
-<style scoped></style>
+<style scoped>
+    h1 {
+        font-size: 175px;
+        font-variant-numeric: normal
+    }
+    .statusBar {
+        height: 30px;
+        line-height: 30px;
+        font-size: 12px;
+        position: fixed;
+        width: 100%;
+        background-color: #eeeeee;
+        border-bottom: 1px solid #ccc 
+    }
+    .game {
+        margin: 0;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%)
+    }
+    .game p {
+        position: relative;
+        top: -29px;
+        letter-spacing: 6px;
+        font-size: 23px
+    }
+    button {
+        letter-spacing: 3px;
+        text-transform: uppercase;
+        background-color: #4CAF50;
+        border: none;
+        color: white;
+        padding: 14px 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 15px
+    }
+</style>
--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
-const express = require("express")();
-const http = require("http").Server(express);
-const socketio = require("socket.io")(http);
+const express = require('express')();
+const http = require('http').Server(express);
+const io = require('socket.io')(http);
 
-socketio.on("connection", socket => {
-    console.log("New connection", socket.id);
+io.on('connection', socket => {
+    io.emit('client count', io.engine.clientsCount);
+    socket.emit('update points', 20);
+    // Functions
+    socket.on('play', (points) => {
+        points--;
+        socket.emit('update points', points);
+    });
 });
 
 http.listen(3000, () => {
-    console.log("Listening at :3000...");
+    console.log('Listening at: 3000');
 });
